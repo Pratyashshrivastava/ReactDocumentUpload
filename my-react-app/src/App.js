481815cb50d30bfa6b44c50f9cf409827d5dde79
Pratyashshrivastava <pratyash.shrivastava2021@vitstudent.ac.in>
2023-10-10 16:38:30 +0530

    ...

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,15 +1,80 @@
-import logo from "./logo.svg";
-import AddNewLeaveRequest from "./LeaveRequestForm";
-import "./LeaveRequestForm.css"; // Import the CSS file
-import DocumentUploadForm from "./documentUpload";
+// import logo from "./logo.svg";
+// import AddNewLeaveRequest from "./LeaveRequestForm";
+// import "./LeaveRequestForm.css"; // Import the CSS file
+// import DocumentUploadForm from "./documentUpload";
+
+// function App() {
+//   return (
+//     <div>
+//       <h1>Leave Request Form</h1>
+//       <DocumentUploadForm />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+/*App.js*/
+
+/*App.js*/
+
+import React, { useState, useEffect } from "react";
+import { googleLogout, useGoogleLogin } from "@react-oauth/google";
+import axios from "axios";
 
 function App() {
+  const [user, setUser] = useState([]);
+  const [profile, setProfile] = useState([]);
+
+  const login = useGoogleLogin({
+    onSuccess: (codeResponse) => setUser(codeResponse),
+    onError: (error) => console.log("Login Failed:", error),
+  });
+
+  useEffect(() => {
+    if (user) {
+      axios
+        .get(
+          `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`,
+          {
+            headers: {
+              Authorization: `Bearer ${user.access_token}`,
+              Accept: "application/json",
+            },
+          }
+        )
+        .then((res) => {
+          setProfile(res.data);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [user]);
+
+  // log out function to log the user out of google and set the profile array to null
+  const logOut = () => {
+    googleLogout();
+    setProfile(null);
+  };
+
   return (
     <div>
-      <h1>Leave Request Form</h1>
-      <DocumentUploadForm />
+      <h2>React Google Login</h2>
+      <br />
+      <br />
+      {profile ? (
+        <div>
+          <img src={profile.picture} alt="user image" />
+          <h3>User Logged in</h3>
+          <p>Name: {profile.name}</p>
+          <p>Email Address: {profile.email}</p>
+          <br />
+          <br />
+          <button onClick={logOut}>Log out</button>
+        </div>
+      ) : (
+        <button onClick={() => login()}>Sign in with Google 🚀 </button>
+      )}
     </div>
   );
 }
-
 export default App;
